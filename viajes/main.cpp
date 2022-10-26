d1f2b44e7f940a3f5fb798f679f5f903f9498e24
albertoleandrosanchez <albertoleandrosanchez@hotmail.com>
2022-10-26 16:43:23 -0300

    empezando camino minimo

--- viajes/main.cpp
+++ viajes/main.cpp
@@ -13,62 +13,84 @@
 void cargarNodos(Grafo *grafo);
 void cargarAristas(Grafo *grafo);
 void inicializarGrafo(Grafo *grafo);
+void calcularCaminoMinimo(Grafo *grafo) void mostrarMenu();
 
 int main()
 {
-    Grafo *grafo = new Grafo();
-    inicializarGrafo(grafo);
-    grafo->verNodos();
+  int opcion;
+  Grafo *grafo = new Grafo();
+  inicializarGrafo(grafo);
 
-    grafo->~Grafo();
-    return 0;
+  do
+  {
+    mostrarMenuPrincipal(Grafo * grafo);
+    cin << opcion;
+    switch (opcion)
+    {
+    case 1:
+      grafo->verNodos();
+      break;
+    case 2:
+      calcularCaminoMinimo(*grafo);
+      break;
+    }
+  } while (opcion != 0);
+
+  grafo->~Grafo();
+  return 0;
 }
 
 void inicializarGrafo(Grafo *grafo)
 {
-    cargarNodos(grafo);
-    cargarAristas(grafo);
+  cargarNodos(grafo);
+  cargarAristas(grafo);
 }
 
 void cargarNodos(Grafo *grafo)
 {
-    ifstream archivo;
-    string linea;
-    string cod, nombre, ciudad, pais;
-    double superficie;
-    int cantidadTerminales, destinosNacionales, destinosInternacionales;
+  ifstream archivo;
+  string linea;
+  string cod, nombre, ciudad, pais;
+  double superficie;
+  int cantidadTerminales, destinosNacionales, destinosInternacionales;
 
-    archivo.open("terminales.txt", ios::in);
-    if (archivo.fail())
-    {
-        cout << "No se pudo abrir el archivo";
-        exit(1);
-    }
-    while (!archivo.eof())
-    {
-        archivo >> cod >> nombre >> ciudad >> pais >> superficie >> cantidadTerminales >> destinosNacionales >> destinosInternacionales;
-        grafo->agregarNodo(new NodoG(cod, ciudad));
-    }
-    archivo.close();
+  archivo.open("terminales.txt", ios::in);
+  if (archivo.fail())
+  {
+    cout << "No se pudo abrir el archivo";
+    exit(1);
+  }
+  while (!archivo.eof())
+  {
+    archivo >> cod >> nombre >> ciudad >> pais >> superficie >> cantidadTerminales >> destinosNacionales >> destinosInternacionales;
+    grafo->agregarNodo(new NodoG(cod, ciudad));
+  }
+  archivo.close();
 }
 
 void cargarAristas(Grafo *grafo)
 {
-    ifstream archivo;
-    string linea;
-    string codOrigen, codDestino;
-    double distancia, horasDeViaje;
-    archivo.open("viajes.txt", ios::in);
-    if (archivo.fail())
-    {
-        cout << "No se pudo abrir el archivo";
-        exit(1);
-    }
+  ifstream archivo;
+  string linea;
+  string codOrigen, codDestino;
+  double distancia, horasDeViaje;
+  archivo.open("viajes.txt", ios::in);
+  if (archivo.fail())
+  {
+    cout << "No se pudo abrir el archivo";
+    exit(1);
+  }
 
-    while (!archivo.eof())
-    {
-        archivo >> codOrigen >> codDestino >> distancia >> horasDeViaje;
-        grafo->obtenerNodo(codOrigen)->agregarArista(grafo->obtenerNodo(codDestino), distancia, horasDeViaje);
-    }
-    archivo.close();
+  while (!archivo.eof())
+  {
+    archivo >> codOrigen >> codDestino >> distancia >> horasDeViaje;
+    grafo->obtenerNodo(codOrigen)->agregarArista(grafo->obtenerNodo(codDestino), distancia, horasDeViaje);
+  }
+  archivo.close();
+}
+
+void mostrarMenuPrincipal(Grafo *grafo))
+{
+  cout << "1. Mostrar todos los nodos del grafo" << endl;
+  cout << "2. Mostrar Recorrido minimo de un nodo a otro" << endl;
 }
